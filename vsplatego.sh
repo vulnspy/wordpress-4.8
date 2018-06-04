e4c9a942b5921d1ee513a4bef3472093cf09e60b
Ambulong <zeng.ambulong@gmail.com>
2018-06-04 22:24:44 +0800

    create db

--- vsplatego.sh
+++ vsplatego.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mysql -u root -ptoor -e 'create database wordpress;';
 tar -zxf /var/www/html/wordpress-4.8.tar.gz -C /var/www/html;
 rm -rf /var/www/html/wordpress-4.8.tar.gz;
 chmod -R 777 /var/www/html;
