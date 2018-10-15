23a57cd9c8fd0c61f8e764f2c5fde861d1e2430c
Ambulong <zeng.ambulong@gmail.com>
2018-10-16 00:01:21 +0800

    docker-compose

--- vsplatego.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-service mysqld start || service mysql start && mysql -u root -ptoor -e 'create database wordpress;';
-wget -O /var/www/html/wordpress-4.8.tar.gz http://test-1251457471.cosgz.myqcloud.com/wordpress-4.8.tar.gz && tar -zxf /var/www/html/wordpress-4.8.tar.gz -C /var/www/html;
-rm -rf /var/www/html/wordpress-4.8.tar.gz;
-chmod -R 777 /var/www/html;
